594cb25fe3f356c5b800cb5ab6b3a5c040b0d84a
Tom White <tom@sixdozen.com>
2016-10-01 01:42:42 +1300

    Added atvec and facegrid scripts (from discgen)

---
+++ atvec.py
@@ -0,0 +1,249 @@
+import argparse
+import sys
+import json
+import numpy as np
+from chips.fuel_helper import get_dataset_iterator
+from plat.utils import json_list_to_array
+
+def get_averages(attribs, encoded, num_encoded_attributes):
+    with_attr = [[] for x in xrange(num_encoded_attributes)]
+    without_attr = [[] for x in xrange(num_encoded_attributes)]
+    for i in range(len(encoded)):
+        if i % 10000 == 0:
+            print("iteration {}".format(i))
+        for m in range(num_encoded_attributes):
+            if attribs[i][0][m] == 1:
+                with_attr[m].append(encoded[i])
+            else:
+                without_attr[m].append(encoded[i])
+
+    print("With: {}".format(map(len, with_attr)))
+    print("Without: {}".format(map(len, without_attr)))
+
+    with_attr = map(np.array, with_attr)
+    without_attr = map(np.array, without_attr)
+    return with_attr, without_attr
+
+def get_balanced_averages2(attribs, encoded, a1, a2):
+    just_a1 = []
+    just_a2 = []
+    both = []
+    neither = []
+    for i in range(len(encoded)):
+        if i % 10000 == 0:
+            print("iteration {}".format(i))
+        if attribs[i][0][a1] == 1 and attribs[i][0][a2] == 1:
+            both.append(encoded[i])
+        elif attribs[i][0][a1] == 1 and attribs[i][0][a2] == 0:
+            just_a1.append(encoded[i])
+        elif attribs[i][0][a1] == 0 and attribs[i][0][a2] == 1:
+            just_a2.append(encoded[i])
+        elif attribs[i][0][a1] == 0 and attribs[i][0][a2] == 0:
+            neither.append(encoded[i])
+        else:
+            print("DANGER: ", attribs[i][0][a1], attribs[i][0][a2])
+
+    len_both = len(both)
+    len_just_a1 = len(just_a1)
+    len_just_a2 = len(just_a2)
+    len_neither = len(neither)
+    topnum = max(len_both, len_just_a1, len_just_a2, len_neither)
+
+    print("max={}, both={}, a1={}, a2={}, neither={}".format(
+        topnum, len_both, len_just_a1, len_just_a2, len_neither))
+
+    just_a1_bal = []
+    just_a2_bal = []
+    both_bal = []
+    neither_bal = []
+
+    for i in range(topnum):
+        both_bal.append(both[i%len_both])
+        just_a1_bal.append(just_a1[i%len_just_a1])
+        just_a2_bal.append(just_a2[i%len_just_a2])
+        neither_bal.append(neither[i%len_neither])
+
+    with_attr = [ (just_a1_bal + both_bal), (just_a2_bal + both_bal)  ]
+    without_attr = [ (just_a2_bal + neither_bal), (just_a1_bal + neither_bal) ]
+
+    print("With: {}".format(map(len, with_attr)))
+    print("Without: {}".format(map(len, without_attr)))
+
+    with_attr = map(np.array, with_attr)
+    without_attr = map(np.array, without_attr)
+    return with_attr, without_attr
+
+# recursive function to initialize nested array
+def nested_binary_array_init(level, leaf_value):
+    if level == 0:
+        if leaf_value == None:
+            return []
+        else:
+            return leaf_value
+    else:
+        return [
+            nested_binary_array_init(level-1, leaf_value),
+            nested_binary_array_init(level-1, leaf_value)
+        ]
+
+# recursive function to assign all lengths and return largest
+def assign_len_get_max(matrix, lengths):
+    if lengths[0] == 0:
+        # leaf
+        lengths[0] = len(matrix[0])
+        lengths[1] = len(matrix[1])
+        if lengths[0] > lengths[1]:
+            return lengths[0]
+        else:
+            return lengths[1]
+    else:
+        # recursive
+        len1 = assign_len_get_max(matrix[0], lengths[0])
+        len2 = assign_len_get_max(matrix[1], lengths[1])
+        if len1 > len2:
+            return len1
+        else:
+            return len2
+
+def replicate_balance_matrix(matrix, lengths, max_len):
+    if isinstance(lengths[0], list):
+        # recurse
+        m1 = replicate_balance_matrix(matrix[0], lengths[0], max_len)
+        m2 = replicate_balance_matrix(matrix[1], lengths[1], max_len)
+        return [m1, m2]
+    else:
+        # compute leaves
+        m1 = []
+        m2 = []
+        for z in range(max_len):
+            m1.append(matrix[0][z%lengths[0]])
+            m2.append(matrix[1][z%lengths[1]])
+        return [m1, m2]
+
+def collect_samples(branch, matrix, depth_decide, cur_depth):
+    if isinstance(matrix[0][0], list):
+        if cur_depth == depth_decide:
+            return collect_samples(branch, matrix[branch], depth_decide, cur_depth+1);
+        else:
+            s1 = collect_samples(branch, matrix[0], depth_decide, cur_depth+1);
+            s2 = collect_samples(branch, matrix[1], depth_decide, cur_depth+1);
+            return s1 + s2
+    else:
+        if cur_depth == depth_decide:
+            # slice syntax -> return a copy
+            return matrix[branch][:]
+        else:
+            return matrix[0] + matrix[1]
+
+def get_balanced_averages(attribs, encoded, indexes):
+    num_ix = len(indexes)
+
+    # initialize 2^n arrays.
+    matrix = nested_binary_array_init(num_ix, None)
+    lengths = nested_binary_array_init(num_ix, 0)
+
+    # great, now partition the encoded list according to those attribs
+    for i in range(len(encoded)):
+        a = attribs[i][0]
+        m = matrix
+        for j in range(num_ix):
+            m = m[a[indexes[j]]]
+        m.append(encoded[i])
+
+    max_len = assign_len_get_max(matrix, lengths)
+
+    print("max={}, both={}, a1={}, a2={}, neither={}".format(
+        max_len, lengths[1][1], lengths[1][0], lengths[0][1], lengths[0][0]))
+
+    matrix_bal = replicate_balance_matrix(matrix, lengths, max_len)
+
+    with_attr = [[] for i in range(num_ix)]
+    without_attr = [[] for i in range(num_ix)]
+
+    for i in range(num_ix):
+        without_attr[i] = collect_samples(0, matrix_bal, i, 0)
+        with_attr[i] = collect_samples(1, matrix_bal, i, 0)
+
+    print("With: {}".format(map(len, with_attr)))
+    print("Without: {}".format(map(len, without_attr)))
+
+    with_attr = map(np.array, with_attr)
+    without_attr = map(np.array, without_attr)
+
+    return with_attr, without_attr
+
+def averages_to_attribute_vectors(with_attr, without_attr, num_encoded_attributes, latent_dim):
+    atvecs = np.zeros((num_encoded_attributes, latent_dim))
+    for n in range(num_encoded_attributes):
+        m1 = np.mean(with_attr[n],axis=0)
+        m2 = np.mean(without_attr[n],axis=0)
+        atvecs[n] = m1 - m2
+    return atvecs
+
+#TODO: switch to plat.save_json_vectors
+def save_json_attribs(attribs, filename):
+    with open(filename, 'w') as outfile:
+        json.dump(attribs.tolist(), outfile)   
+
+def main(cliargs):
+    parser = argparse.ArgumentParser(description="Plot model samples")
+    parser.add_argument('--dataset', dest='dataset', default=None,
+                        help="Source dataset (for labels).")
+    parser.add_argument('--split', dest='split', default="train",
+                        help="Which split to use from the dataset (train/nontrain/valid/test/any).")
+    parser.add_argument("--num-attribs", dest='num_attribs', type=int, default=40,
+                        help="Number of attributes (labes)")
+    parser.add_argument("--z-dim", dest='z_dim', type=int, default=100,
+                        help="z dimension of vectors")
+    parser.add_argument("--encoded-vectors", type=str, default=None,
+                        help="Comma separated list of json arrays")
+    parser.add_argument("--balanced2", dest='balanced2', type=str, default=None,
+                        help="Balanced two attributes and generate atvec. eg: 20,31")
+    parser.add_argument("--balanced", dest='balanced', type=str, default=None,
+                        help="Balance attributes and generate atvec. eg: 20,21,31")
+    parser.add_argument("--avg-diff", dest='avg_diff', type=str, default=None,
+                        help="Two lists of vectors to average and then diff")
+    parser.add_argument('--outfile', dest='outfile', default=None,
+                        help="Output json file for vectors.")
+    args = parser.parse_args(cliargs)
+
+    if args.avg_diff:
+        vecs1, vecs2 = args.avg_diff.split(",")
+        encoded1 = json_list_to_array(vecs1)
+        encoded2 = json_list_to_array(vecs2)
+        print("Taking the difference between {} and {} vectors".format(len(encoded1), len(encoded2)))
+        m1 = np.mean(encoded1,axis=0)
+        m2 = np.mean(encoded2,axis=0)
+        atvec = m2 - m1
+        z_dim, = atvec.shape
+        atvecs = atvec.reshape(1,z_dim)
+        print("Computed diff shape: {}".format(atvecs.shape))
+        if args.outfile is not None:
+            save_json_attribs(atvecs, args.outfile)
+        sys.exit(0)
+
+    encoded = json_list_to_array(args.encoded_vectors)
+    num_rows, z_dim = encoded.shape
+    attribs = np.array(list(get_dataset_iterator(args.dataset, args.split, include_features=False, include_targets=True)))
+    print("encoded vectors: {}, attributes: {} ".format(encoded.shape, attribs.shape))
+
+    if(args.balanced2):
+        indexes = map(int, args.balanced2.split(","))
+        with_attr, without_attr = get_balanced_averages2(attribs, encoded, indexes[0], indexes[1]);
+        num_attribs = 2
+    elif(args.balanced):
+        indexes = map(int, args.balanced.split(","))
+        with_attr, without_attr = get_balanced_averages(attribs, encoded, indexes);
+        num_attribs = len(indexes)
+    else:
+        with_attr, without_attr = get_averages(attribs, encoded, args.num_attribs);
+        num_attribs = args.num_attribs
+
+    atvects = averages_to_attribute_vectors(with_attr, without_attr, num_attribs, z_dim)
+    print("Computed atvecs shape: {}".format(atvects.shape))
+
+    if args.outfile is not None:
+        save_json_attribs(atvects, args.outfile)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ facegrid.py
@@ -0,0 +1,221 @@
+"""Does facegrid stuff."""
+import argparse
+
+import numpy as np
+import os
+import json
+import sys
+
+from PIL import Image
+from annoy import AnnoyIndex
+from sklearn.manifold import TSNE
+from fuel_helper import get_anchor_images
+from plat.utils import anchors_from_image, json_list_to_array
+
+def build_annoy_index(encoded, outfile):
+    input_shape = encoded.shape
+    f = input_shape[1]
+    t = AnnoyIndex(f, metric='angular')  # Length of item vector that will be indexed
+    for i,v in enumerate(encoded):
+        t.add_item(i, v)
+
+    t.build(100) # 10 trees
+    if outfile is not None:
+        t.save(outfile)
+
+    return t
+
+def load_annoy_index(infile, z_dim):
+    t = AnnoyIndex(z_dim)
+    t.load(infile) # super fast, will just mmap the file
+    return t
+
+def neighbors_to_grid(neighbors, imdata, gsize, with_center=False):
+    canvas = np.zeros((gsize*3, gsize*5, 3)).astype(np.uint8)
+    if with_center:
+        offsets = [ [0, 0] ]
+    else:
+        offsets = []
+    offsets += [
+        [-1, 0],
+        [0, -1],
+        [1, 0],
+        [0, 1],
+        [-1, -1],
+        [-1, 1],
+        [1, -1],
+        [1, 1],
+        [0, -2],
+        [0, 2],
+        [-1, -2],
+        [1, -2],
+        [-1, 2],
+        [1, 2]
+    ]
+    cx = gsize*2
+    cy = gsize*1
+    for i, offset in enumerate(offsets):
+        n = neighbors[i]
+        im = np.dstack(imdata[n]).astype(np.uint8)
+        offy = cy + gsize*offset[0]
+        offx = cx + gsize*offset[1]
+        canvas[offy:offy+gsize, offx:offx+gsize, :] = im
+    return Image.fromarray(canvas)
+
+def debug_save_plot(xy, filename):
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_axis_bgcolor('black')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.autoscale_view(True,True,True)
+    ax.invert_yaxis()
+    ax.scatter(xy[:,0],xy[:,1],  edgecolors='none',marker='s',s=7.5)  # , c = vectors[:,:3]
+    plt.savefig(filename)
+
+def neighbors_to_rfgrid(neighbors, encoded, imdata, gsize, gridw, gridh):
+    canvas = np.zeros((gsize*gridh, gsize*gridw, 3)).astype(np.uint8)
+
+    vectors_list = []
+    for n in neighbors:
+        vectors_list.append(encoded[n])
+    vectors = np.array(vectors_list)
+
+    RS = 20150101
+    # xy = bh_sne(vectors, perplexity=4., theta=0)
+    xy = TSNE(init='pca', learning_rate=100, random_state=RS, method='exact', perplexity=4).fit_transform(vectors)
+
+    # debug_save_plot(xy, "plot.png")
+
+    from rasterfairy import rasterfairy
+    grid_xy, quadrants = rasterfairy.transformPointCloud2D(xy,target=(gridw,gridh))
+    indices = []
+    for i in range(gridw * gridh):
+        indices.append(quadrants[i]["indices"][0])
+
+    i = 0
+    for cur_y in range(gridh):
+        for cur_x in range(gridw):
+            cur_index = indices[i]
+            n = neighbors[cur_index]
+            im = np.dstack(imdata[n]).astype(np.uint8)
+            offy = gsize * cur_y
+            offx = gsize * cur_x
+            canvas[offy:offy+gsize, offx:offx+gsize, :] = im
+            i = i + 1
+
+    return Image.fromarray(canvas)
+
+def main(cliargs):
+    parser = argparse.ArgumentParser(description="Plot model samples")
+    parser.add_argument("--model-module", dest='model_module', type=str,
+                        default="utils.interface", help="module encapsulating model")
+    parser.add_argument("--model-class", dest='model_class', type=str,
+                        default="DiscGenModel", help="class encapsulating model")
+    parser.add_argument('--build-annoy', dest='build_annoy',
+                        default=False, action='store_true')
+    parser.add_argument("--jsons", type=str, default=None,
+                        help="Comma separated list of json arrays")
+    parser.add_argument('--dataset', dest='dataset', default=None,
+                        help="Source dataset.")
+    parser.add_argument('--dataset-image', dest='dataset_image', default=None,
+                        help="use image as source dataset")
+    parser.add_argument("--dataset-offset", dest='dataset_offset', type=int, default=0,
+                        help="dataset offset to skip")
+    parser.add_argument("--dataset-max", type=int, default=None,
+                        help="Source dataset.")
+    parser.add_argument('--seeds-image', dest='seeds_image', default=None,
+                        help="image source of seeds")
+    parser.add_argument("--model", dest='model', type=str, default=None,
+                        help="model for encoding when seeds-images is enabled")
+    parser.add_argument('--annoy-index', dest='annoy_index', default=None,
+                        help="Annoy index.")
+    parser.add_argument('--split', dest='split', default="all",
+                        help="Which split to use from the dataset (train/nontrain/valid/test/any).")
+    parser.add_argument("--image-size", dest='image_size', type=int, default=64,
+                        help="size of (offset) images")
+    parser.add_argument("--z-dim", dest='z_dim', type=int, default=100,
+                        help="z dimension")
+    parser.add_argument('--outdir', dest='outdir', default="neighborgrids",
+                        help="Output dir for neighborgrids.")
+    parser.add_argument('--outfile', dest='outfile', default="index_{:03d}.png",
+                        help="Output file (template) for neighborgrids.")
+    parser.add_argument("--outgrid-width", dest='outgrid_width', type=int, default=5,
+                        help="width of output grid")
+    parser.add_argument("--outgrid-height", dest='outgrid_height', type=int, default=3,
+                        help="height of output grid")
+    parser.add_argument('--range', dest='range', default="0",
+                        help="Range of indexes to run.")
+    args = parser.parse_args(cliargs)
+
+    encoded = json_list_to_array(args.jsons)
+    # print(encoded.shape)
+    if args.build_annoy:
+        aindex = build_annoy_index(encoded, args.annoy_index)
+        sys.exit(0)
+
+    # open annoy index and spit out some neighborgrids
+    aindex = load_annoy_index(args.annoy_index, args.z_dim)
+    if args.dataset is not None:
+        anchor_images = get_anchor_images(args.dataset, args.split, offset=args.dataset_offset, numanchors=args.dataset_max, unit_scale=False)
+        image_size = anchor_images.shape[2]
+    # dataset_image requires image_size
+    if args.dataset_image is not None:
+        image_size = args.image_size
+        _, _, anchor_images = anchors_from_image(args.dataset_image, image_size=(image_size, image_size), unit_scale=False)
+        if args.dataset_offset > 0:
+            anchor_images = anchor_images[args.dataset_offset:]
+        if args.dataset_max is not None:
+            anchor_images = anchor_images[:args.dataset_max]
+
+
+    r = map(int, args.range.split(","))
+
+    core_dataset_size = len(anchor_images)
+    if(len(encoded) != core_dataset_size):
+        print("Warning: {} vectors and {} images".format(len(encoded), core_dataset_size))
+    if args.seeds_image is not None:
+        image_size = args.image_size
+        _, _, extra_images = anchors_from_image(args.seeds_image, image_size=(image_size, image_size), unit_scale=False)
+        net_inputs = (extra_images / 255.0).astype('float32')
+
+        print('Loading saved model...')
+        ModelClass = getattr(importlib.import_module(args.model_module), args.model_class)
+        dmodel = ModelClass(filename=args.model)
+
+        image_vectors = dmodel.encode_images(net_inputs)
+        num_extras = len(extra_images)
+        encoded = np.concatenate((encoded, image_vectors), axis=0)
+        anchor_images = np.concatenate((anchor_images, extra_images), axis=0)
+        # for now, override given range
+        r = [core_dataset_size, core_dataset_size + num_extras]
+
+    print anchor_images.shape
+
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+
+    if len(r) == 1:
+        r = [r[0], r[0]+1]
+    num_out_cells = args.outgrid_width * args.outgrid_height
+    for i in range(r[0], r[1]):
+        if i < core_dataset_size:
+            neighbors = aindex.get_nns_by_item(i, num_out_cells, include_distances=True) # will find the 20 nearest neighbors
+            file_num = i
+        else:
+            neighbors = aindex.get_nns_by_vector(encoded[i], num_out_cells-1, include_distances=True) # will find the 20 nearest neighbors
+            neighbors[0].append(i)
+            neighbors[1].append(0)
+            file_num = i - core_dataset_size
+
+        g = neighbors_to_rfgrid(neighbors[0], encoded, anchor_images, image_size, args.outgrid_width, args.outgrid_height)
+        out_template = "{}/{}".format(args.outdir, args.outfile)
+        g.save(out_template.format(file_num))
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
