09d5e0593ff534c5eb417b016842e5d564c0400b
Tom White <tom@sixdozen.com>
2016-10-06 13:53:24 +1300

    Rolled version to 0.2.1

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ install_requires = [
 ]
 
 setup(name='plat',
-      version='0.2.0',
+      version='0.2.1',
       description='Utilities for working with generative latent spaces',
       author='Tom White',
       author_email='tom@sixdozen.com',
       url='https://github.com/dribnet/plat',
-      download_url='https://github.com/dribnet/plat/archive/0.2.0.tar.gz',
+      download_url='https://github.com/dribnet/plat/archive/0.2.1.tar.gz',
       license='MIT',
       entry_points={
           'console_scripts': ['plat = plat.bin.platcmd:main']
