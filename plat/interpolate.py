1a7eeacaeed534aed183478ae4c87c721ea0416e
Tom White <tom@sixdozen.com>
2017-04-03 00:50:58 +1200

    interpolate: added error check case to slerp

--- plat/interpolate.py
+++ plat/interpolate.py
@@ -18,6 +18,8 @@ def slerp(val, low, high):
         return low
     elif val >= 1:
         return high
+    elif np.allclose(low, high):
+        return low
     omega = np.arccos(np.dot(low/np.linalg.norm(low), high/np.linalg.norm(high)))
     so = np.sin(omega)
     return np.sin((1.0-val)*omega) / so * low + np.sin(val*omega)/so * high
